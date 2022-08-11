fbd282988dc8c98c8796be258616801d208906a7
Rubixchamp07 <88152130+Rubixchamp07@users.noreply.github.com>
2022-08-11 15:37:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function preload()
 {
 
@@ -25,10 +31,23 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor( leftWristX - rightWristX );
+
+        console.log('leftWristX = ' + leftWristX + 'rightWristX = ' + rightWristX + 'difference = '+ difference);
     }
 }
 
 function draw()
 {
     background('#00FFFF');
+    document.getElementById('text_side').innerHTML = 'Width and Height of the text will be = ' + difference + 'px';
+    fill('#ff0000');
+    stroke('#32CD32');
+    text(noseX, noseY, difference);
+
 }
